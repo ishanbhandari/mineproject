9419540ae38bfa2cfd66335dd8552e4afc591407
ishanbhandari <39106720+ishanbhandari@users.noreply.github.com>
2018-06-05 19:08:30 -0400

    Add files via upload

---
+++ project/Ishan.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+      package ishan;
+
+/**
+ *
+ * @author macstudent
+ */
+       public class Ishan {
+
+    public static void main(String[] args) {
+        
+        student s1 = new student();
+        s1.display();
+        
+        person p1 = new person();
+        p1.display();
+        
+        p1 = s1;
+        p1.display();
+        
+        
+        
+        
+           
+    }
+    
+}
+    
+    
